5c60741c119b883770d6ae57403a3151f291cf84
Victor Hugo <victor.hugo.ina10@gmail.com>
2025-02-03 18:11:22 -0300

    variaveis de ambiente para teste e produção criadas usando dotenv

--- db.js
+++ db.js
@@ -1,10 +1,48 @@
 import mysql from "mysql2";
+import dotenv from 'dotenv';
 
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "dashboard"
-})
+// configura as variaveis de ambiante
+dotenv.config();
+
+// define a url da database e da localdatabase
+const dbUrl = process.env.DATABASE_URL;
+const dbLocalUrl = process.env.DATABASE_LOCAL;
+
+// cria uma variavel para usar a database
+let db; 
+
+// função para conectara adata base e reconectara, caso de erro ou desconexão
+const handleConect = () => {
+
+  // define o banco de dados como local se nao existir url no arquivo .env
+  if (dbUrl.length) {
+    db = mysql.createConnection(dbUrl)
+  } else {
+    db = mysql.createConnection(dbLocalUrl)
+  }
+
+  // reconecta a database caso a aconexão seja perdida
+  db.connect((error)=>{
+    if (error) {
+      console.error("Erro ao conectar a Data Base: ", error)
+      setTimeout(handleConect, 2000) // reconecta a database apos 2segundos
+    } else {
+      console.log("Conectado a Data Base!")
+    }
+  })
+
+  // reconecta ou informa o erro caso a conexão seja negada
+  db.on((error)=>{
+    console.error("Erro na Data Base: ", error)
+    if (error.code === 'PROTOCOL_CONNECTION_LOST' || error.fatal) {
+      handleConect();
+    } else {
+      throw error;
+    }
+  })
+}
+
+// inicia a função de conectar a database
+handleConect();
 
 export default db;
