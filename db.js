aa3fe422482262d9570ce05a0c19f0efa123f54e
Victor Hugo <victor.hugo.ina10@gmail.com>
2025-02-03 21:19:57 -0300

    feat: db.js alterado

--- db.js
+++ db.js
@@ -1,48 +1,47 @@
 import mysql from "mysql2";
 import dotenv from 'dotenv';
 
-// configura as variaveis de ambiante
+// configura as variáveis de ambiente
 dotenv.config();
 
-// define a url da database e da localdatabase
+// define a URL da database e da localdatabase
 const dbUrl = process.env.DATABASE_URL;
 const dbLocalUrl = process.env.DATABASE_LOCAL;
 
-// cria uma variavel para usar a database
-let db; 
+// cria uma variável para usar a database
+let db;
 
-// função para conectara adata base e reconectara, caso de erro ou desconexão
+// função para conectar à database e reconectar, caso de erro ou desconexão
 const handleConect = () => {
-
-  // define o banco de dados como local se nao existir url no arquivo .env
-  if (dbUrl.length) {
-    db = mysql.createConnection(dbUrl)
+  // define o banco de dados como local se não existir URL no arquivo .env
+  if (dbUrl) {
+    db = mysql.createConnection(dbUrl);
   } else {
-    db = mysql.createConnection(dbLocalUrl)
+    db = mysql.createConnection(dbLocalUrl);
   }
 
-  // reconecta a database caso a aconexão seja perdida
-  db.connect((error)=>{
+  // reconecta à database caso a conexão seja perdida
+  db.connect((error) => {
     if (error) {
-      console.error("Erro ao conectar a Data Base: ", error)
-      setTimeout(handleConect, 2000) // reconecta a database apos 2segundos
+      console.error("Erro ao conectar à Data Base: ", error);
+      setTimeout(handleConect, 2000); // reconecta à database após 2 segundos
     } else {
-      console.log("Conectado a Data Base!")
+      console.log("Conectado à Data Base!");
     }
-  })
+  });
 
   // reconecta ou informa o erro caso a conexão seja negada
-  db.on('error', (error)=>{
-    console.error("Erro na Data Base: ", error)
+  db.on('error', (error) => {
+    console.error("Erro na Data Base: ", error);
     if (error.code === 'PROTOCOL_CONNECTION_LOST' || error.fatal) {
       handleConect();
     } else {
       throw error;
     }
-  })
-}
+  });
+};
 
-// inicia a função de conectar a database
+// inicia a função de conectar à database
 handleConect();
 
 export default db;
