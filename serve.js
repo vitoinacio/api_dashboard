706b6a4380a03d9311b225d3bc637d297f2a97e1
VICTOR HUGO INACIO DE OLIVEIRA <victor.oliveira@petronect.com.br>
2025-03-03 18:44:36 -0300

    feat: cors install

--- serve.js
+++ serve.js
@@ -2,9 +2,16 @@ import fastify from 'fastify';
 import userRouter from './src/routes/user.routes/Users.js';
 import financeiroRouter from './src/routes/financeiro.routes/Financeiro.js';
 import debitosRouter from './src/routes/debitos.routes/Debitos.js';
+import fastifyCors from 'fastify-cors';
 
 const app = fastify();
 app.use(cors());
+
+app.register(fastifyCors, {
+  origin: ['http://localhost:5173'], // Permite apenas requisições do seu frontend
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
+  allowedHeaders: ['Content-Type', 'Authorization'], // Cabeçalhos permitidos
+});
 
 // rota de usuario
 userRouter(app);
