a90a022653a9b5b66fb8775de9c73dbefc77b7f7
VICTOR HUGO INACIO DE OLIVEIRA <victor.oliveira@petronect.com.br>
2025-03-03 18:33:57 -0300

    feat: cors install

--- serve.js
+++ serve.js
@@ -4,6 +4,7 @@ import financeiroRouter from './src/routes/financeiro.routes/Financeiro.js';
 import debitosRouter from './src/routes/debitos.routes/Debitos.js';
 
 const app = fastify();
+app.use(cors());
 
 // rota de usuario
 userRouter(app);
