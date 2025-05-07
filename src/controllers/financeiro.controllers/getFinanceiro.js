1e4ecb28720ed6e10f1b7a67c4f59e1cac4efee5
VICTOR HUGO INACIO DE OLIVEIRA <victor.oliveira@petronect.com.br>
2025-05-07 10:34:47 -0300

    feat: rota de get refatorada

--- src/controllers/financeiro.controllers/getFinanceiro.js
+++ src/controllers/financeiro.controllers/getFinanceiro.js
@@ -1,26 +1,23 @@
 import db from '../../../db.js';
 
 const getFinanceiro = async (req, reply) => {
-  const { id } = req.query; 
-  
+  const { id } = req.query;
+
   let sql = "SELECT * FROM financeiro";
-  let params = []; 
+  let params = [];
 
   if (id) {
-    sql += ' WHERE idUsuarios = $1'; 
+    sql += " WHERE idUsuarios = $1";
     params = [id];
   }
 
   try {
-    const result = await db.query(sql);
-    
-    const response = result.rows;
-
-    reply.status(200).send(response);
+    const result = await db.query(sql, params);
+    reply.status(200).send(result.rows);
   } catch (error) {
     console.error("Erro ao retornar a entrada:", error);
     reply.status(500).send("Erro ao retornar a entrada: " + error.message);
   }
-}
+};
 
 export default getFinanceiro;
