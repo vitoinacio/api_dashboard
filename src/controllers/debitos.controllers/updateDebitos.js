7588e8b3fd6f207a5ba3815dda68b03f2ed9fa78
VICTOR HUGO INACIO DE OLIVEIRA <victor.oliveira@petronect.com.br>
2025-05-19 17:42:36 -0300

    feat: atualização do update debito

--- src/controllers/debitos.controllers/updateDebitos.js
+++ src/controllers/debitos.controllers/updateDebitos.js
@@ -1,34 +1,60 @@
-import db from "../../../db.js"; // Importando a conexão com o banco PostgreSQL
+import db from "../../../db.js";
 
 const updateDebito = async (req, reply) => {
-  // Consulta SQL para atualizar os dados na tabela debitos
+  let setClause = [];
+  let values = [];
+  let counter = 1; 
+
+  if (req.body.identificacao) {
+    setClause.push(`identificacao = $${counter++}`);
+    values.push(req.body.identificacao);
+  }
+  if (req.body.dataVenc) {
+    setClause.push(`dataVenc = $${counter++}`);
+    values.push(req.body.dataVenc);
+  }
+  if (req.body.observacao) {
+    setClause.push(`observacao = $${counter++}`);
+    values.push(req.body.observacao);
+  }
+  if (req.body.valor) {
+    setClause.push(`valor = $${counter++}`);
+    values.push(req.body.valor);
+  }
+  if (req.body.notificacao !== undefined) {
+    setClause.push(`notificacao = $${counter++}`);
+    values.push(req.body.notificacao);
+  }
+  if (req.body.pago !== undefined) {
+    setClause.push(`pago = $${counter++}`);
+    values.push(req.body.pago);
+  }
+  if (req.body.notificacao_enviada !== undefined) {
+    setClause.push(`notificacao_enviada = $${counter++}`);
+    values.push(req.body.notificacao_enviada);
+  }
+
+  // Verifica se há pelo menos um campo para atualizar
+  if (setClause.length === 0) {
+    return reply.status(400).send("Nenhum dado para atualizar.");
+  }
+
+  // Consulta SQL com as colunas dinâmicas
   const sql = `
     UPDATE debito 
-    SET identificacao = $1, dataVenc = $2, observacao = $3, valor = $4, notificacao = $5 
-    WHERE idUsuarios = $6 AND id_deb = $7
+    SET ${setClause.join(", ")} 
+    WHERE idUsuarios = $${counter++} AND id_deb = $${counter}
   `;
 
-  // Array com os valores a serem passados para a consulta SQL
-  const values = [
-    req.body.identificacao,
-    req.body.dataVenc,
-    req.body.observacao,
-    req.body.valor,
-    req.body.notificacao,
-    req.params.id,
-    req.params.id_deb
-  ];
+  // Adiciona os parâmetros do ID na lista de valores
+  values.push(req.params.id, req.params.id_deb);
 
   try {
-    // Executando a consulta no banco de dados PostgreSQL
     await db.query(sql, values);
-
-    // Enviando uma resposta de sucesso
-    reply.status(200).send("Débito atualizado!");
+    reply.status(200).send("Débito atualizado com sucesso!");
   } catch (error) {
-    // Tratamento de erro caso algo dê errado
     console.error("Erro ao atualizar débito:", error);
-    reply.status(500).send("Erro ao atualizar débito: " + error.message); // Mensagem de erro
+    reply.status(500).send("Erro ao atualizar débito: " + error.message);
   }
 }
 
